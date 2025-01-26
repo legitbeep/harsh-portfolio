e132863dc009e4a2b768329014993401893c5ba0
legitbeep <glorytoharsh@gmail.com>
2025-01-26 13:47:06 +0530

    FIX - remove links

--- components/core/projects.tsx
+++ components/core/projects.tsx
@@ -35,7 +35,7 @@ export const Projects = () => {
       <section className="grid gap-4">
         {PROJECTS_DATA.map(({ description, title, dateData }) => {
           return (
-            <a key={Math.random()} href={"#"} target="_blank">
+            <div key={Math.random()}>
               <div className="sm:transition-transform hover:sm:-translate-y-0.5">
                 <div className="group">
                   <div className="relative overflow-hidden rounded-lg p-px shadow-md ring-1 ring-border before:pointer-events-none before:absolute before:-left-48 before:-top-48 before:z-30 before:h-96 before:w-96 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-blue-500 before:opacity-0 before:blur-[100px] before:transition-opacity before:duration-500 after:absolute after:inset-0 after:z-10 after:rounded-[inherit] after:opacity-0 after:transition-opacity after:duration-500 after:[background:_radial-gradient(250px_circle_at_var(--mouse-x)_var(--mouse-y),theme(colors.slate.400),transparent)] before:hover:opacity-20 after:group-hover:opacity-100 dark:bg-zinc-700 dark:ring-0">
@@ -77,7 +77,7 @@ export const Projects = () => {
                   </div>
                 </div>
               </div>
-            </a>
+            </div>
           );
         })}
       </section>
